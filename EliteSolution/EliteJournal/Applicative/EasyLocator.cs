0be9eb0f7fe1bd4238b29e519a0454798c85fab3
ByGGy <ahcben@yahoo.fr>
2015-01-17 14:57:43 +0100

    Added the EasyLocator class (singleton) used to hold the reference to a messagBus, a repository etc...

---
+++ EliteSolution/EliteJournal/Applicative/EasyLocator.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure;
+
+namespace EliteJournal
+{
+    public sealed class EasyLocator
+    {
+        private static readonly EasyLocator instance = new EasyLocator();
+
+        public static EasyLocator Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private MessageBus news;
+
+        private EasyLocator()
+        {
+            this.news = new MessageBus();
+        }
+
+        public MessageBus News
+        {
+            get { return this.news; }
+        }
+    }
+}
