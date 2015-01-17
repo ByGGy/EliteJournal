13e3650ff77419e7e354fb705fef38c0e9c00392
ByGGy <ahcben@yahoo.fr>
2015-01-17 14:59:32 +0100

    Added the HelloMVVM (view first) as a test (should be deleted once real MVVMs are created)

---
+++ EliteSolution/EliteJournal/Presentation/Hello/HelloView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EliteJournal.Presentation
+{
+    /// <summary>
+    /// Interaction logic for HelloView.xaml
+    /// </summary>
+    public partial class HelloView : UserControl
+    {
+        public HelloView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EliteSolution/EliteJournal/Presentation/Hello/HelloViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure;
+
+namespace EliteJournal.Presentation
+{
+    public class HelloViewModel : ViewModel
+    {
+        public string Message
+        {
+            get { return "Hello there !"; }
+        }
+    }
+}
