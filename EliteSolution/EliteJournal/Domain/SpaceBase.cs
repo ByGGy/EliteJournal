445e906760eb141479b803d9be0b4ff9ba10f5bb
ByGGy <ahcben@yahoo.fr>
2015-01-20 21:36:44 +0100

    Added a BaseType (Station or Outpost) to SpaceBase

--- EliteSolution/EliteJournal/Domain/SpaceBase.cs
+++ EliteSolution/EliteJournal/Domain/SpaceBase.cs
@@ -5,9 +5,16 @@ namespace EliteJournal.Domain
     //TODO : Add Governement, Allegiance and Economy data
     public class SpaceBase
     {
+        public enum BaseType
+        {
+            Station,
+            Outpost
+        }
+
         private Guid id;
 
         private string name;
+        private BaseType type;
         private uint distanceFromStar;  //in Ls
 
         private LocalMarket market;
@@ -17,18 +24,21 @@ namespace EliteJournal.Domain
             this.id = Guid.Empty;
 
             this.name = string.Empty;
+            this.type = BaseType.Station;
             this.distanceFromStar = uint.MinValue;
 
             this.market = LocalMarket.Create();
         }
 
-        public static SpaceBase Create(string name)
+        public static SpaceBase Create(string name, BaseType type, uint distanceFromStar)
         {
             SpaceBase newBase = new SpaceBase();
 
             newBase.id = Guid.NewGuid();
 
             newBase.name = name;
+            newBase.type = type;
+            newBase.distanceFromStar = distanceFromStar;
 
             return newBase;
         }
