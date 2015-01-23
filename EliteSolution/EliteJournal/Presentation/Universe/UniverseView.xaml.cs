a21848e92c9a45105054fd2c7c18115b9df1bc81
ByGGy <ahcben@yahoo.fr>
2015-01-23 11:47:23 +0100

    Added the UniverseMVVM which will allow to manage the starSystems

---
+++ EliteSolution/EliteJournal/Presentation/Universe/UniverseView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace EliteJournal.Presentation
+{
+    public partial class UniverseView : UserControl
+    {
+        public UniverseView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EliteSolution/EliteJournal/Presentation/Universe/UniverseViewModel.cs
@@ -0,0 +1,53 @@
+﻿using EliteJournal.Domain;
+using Infrastructure;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace EliteJournal.Presentation
+{
+    public class UniverseViewModel : ViewModel
+    {
+        private List<StarSystem> starSystemCollection;
+        public List<StarSystem> StarSystemCollection
+        {
+            get { return this.starSystemCollection; }
+            set
+            {
+                if (this.starSystemCollection != value)
+                {
+                    this.starSystemCollection = value;
+                    NotifyPropertyChanged("StarSystemCollection");
+                }
+            }
+        }
+
+        private string newName;
+        public string NewName
+        {
+            get { return this.newName; }
+            set
+            {
+                if (this.newName != value)
+                {
+                    this.newName = value;
+                    NotifyPropertyChanged("NewName");
+                }
+            }
+        }
+
+        public ICommand AddSystemCommand { get; set; }
+
+        public UniverseViewModel()
+        {
+            this.StarSystemCollection = EasyLocator.Instance.Universe.StarSystems.ToList();
+
+            this.AddSystemCommand = new SimpleCommand(() =>
+            {
+                EasyLocator.Instance.Universe.CreateStarSystem(this.newName);
+                this.StarSystemCollection = EasyLocator.Instance.Universe.StarSystems.ToList();
+                this.NewName = string.Empty;
+            });
+        }
+    }
+}
